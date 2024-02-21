6b1660b1508e1d81de7afa79ebce4d448221cca7
armangral <91218206+armangral@users.noreply.github.com>
2024-02-21 22:36:35 +0500

    backend edit post

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -125,3 +125,22 @@ def delete_post(id:int,db:Session=Depends(database.get_db),current_user:int = De
     post_query.delete(synchronize_session=False)
     db.commit()
     return Response(status_code = status.HTTP_204_NO_CONTENT)
+
+
+
+@router.put("/postedit/{id}",response_model=schemas.Post)
+def update_post(id:int,updated_post:schemas.PostCreate,db:Session=Depends(database.get_db),current_user:int = Depends(oauth2.get_current_user)):
+    
+    # cursor.execute("""UPDATE posts SET title = %s ,content = %s ,published = %s WHERE id = %s RETURNING *"""
+    #                , (post.title,post.content,post.published,str(id)))
+    # updated_post = cursor.fetchone()
+    # conn.commit()
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if(post == None):
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail = f"Post with id {id} not Found!")
+    if post.owner_id!=current_user.id:
+        raise HTTPException(status_code = status.HTTP_403_FORBIDDEN,detail = f"Not Authorized to perform this Action!")
+    post_query.update(updated_post.dict(),synchronize_session=False)
+    db.commit()
+    return post_query.first()
